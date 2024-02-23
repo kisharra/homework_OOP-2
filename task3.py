5beb3574b7ae5b703db384cd5fd235c49a054eab
kisharra <djagisama123@gmail.com>
2024-02-23 11:37:45 +0000

    corrections for task No. 3

--- task3.py
+++ task3.py
@@ -1,19 +1,18 @@
-def read_file(file_name):  #функция для считывания
-    with open(file_name, 'r') as file:
-        content = file.readlines()
-    return content
+files = ['1.txt', '2.txt', '3.txt']  # Создаем список файлов вручную
 
-def write_united(file_names, output_file):  #функция для записи
-    with open(output_file, 'w') as output:
-        for file_name in file_names:
-            content = read_file(file_name)
-            output.write(f'{file_name}\n')
-            output.write(f'{len(content)}\n')
-            output.write('\n')
-            output.writelines(content)
-            output.write("\n\n")
+def count_lines(file_name):  # Определяем функцию для получения количества строк в файле
+    with open(file_name, 'r') as f:
+        return sum(1 for line in f)
 
-file_names = ['1.txt', '2.txt', '3.txt']  #файлы
-output_file = 'united_file.txt'  #выходной файл
+files.sort(key=count_lines)  # Сортируем файлы по количеству строк в них
 
-write_united(file_names, output_file)  #объявление функции
+with open('united_file.txt', 'w') as united_file:  # Открываем новый файл для записи
+    for file_name in files:  # Обходим отсортированные файлы
+        united_file.write(f'{file_name}\n')  # Записываем служебную информацию (имя файла и количество строк)
+        united_file.write(f'{count_lines(file_name)}\n')
+        
+        with open(file_name, 'r') as current_file:  # Записываем содержимое файла
+            for line in current_file:
+                united_file.write(line)
+
+        united_file.write('\n' + '\n')
